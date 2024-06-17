e9ce2826cb9a3a5793ad600748e327e945d0b745
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-06-16 21:22:41 -0300

    js increment

--- Alura/Lógica de Programação/Básico/Jogo - Adivinhe o Número/main.js
+++ Alura/Lógica de Programação/Básico/Jogo - Adivinhe o Número/main.js
@@ -1,10 +1,10 @@
 // Settings
-const notLevel = document.querySelector("#not-level");
-const ease = document.querySelector("#ease");
-const medium = document.querySelector("#medium");
-const hard = document.querySelector("#hard");
-const rangeMin = document.querySelector("#rangeMin");
-const rangeMax = document.querySelector("#rangeMax");
+const notLevel = document.querySelector("#not-level");  // *
+const ease = document.querySelector("#ease");  // *
+const medium = document.querySelector("#medium"); // *
+const hard = document.querySelector("#hard"); // *
+const rangeMin = document.querySelector("#rangeMin"); // *
+const rangeMax = document.querySelector("#rangeMax"); // *
 const qtdNumbers = document.querySelector("#qtdNumbers");
 const number = document.querySelector("#number");
 
@@ -12,7 +12,11 @@ const number = document.querySelector("#number");
 const play = document.querySelector("playButton");
 
 // info
+const messageRange = document.querySelector(".messageRange")
+const levelTitle = document.querySelector("#levelTitle")
+const messageLevel = document.querySelector(".messageLevel");
 const gameInfo = document.querySelector(".info");
+const rest = document.querySelector("#rest");
 const atualRange = document.querySelector(".atualRange");
 const tableNumbers = document.querySelector(".table")
 const attemptsMade = document.querySelector(".attemptsMade");
@@ -21,15 +25,111 @@ const clock = document.querySelector(".clock");
 const bestTimeBox = document.querySelector(".bestTimeBox");
 
 const levels = ["not-level", "ease", "medium", "hard"];
+const messages = [messageLevel, messageRange];
 
 let gameSettings = {
     level: "not-level",
     range: {
-        min: 0,
+        min: 1,
         max: 50,
     },
     numbersSorted: 1,
-}
+    attempts: "~",
+};
+
+notLevel.addEventListener('click', function (){
+    gameSettings = {
+        level: "not-level",
+        range: {
+            min: 1,
+            max: 50,
+        },
+        numbersSorted: 1,
+        attempts: "~",
+    };
+    notLevel.classList.add("selected");
+    levelTitle.classList.remove("selected");
+});
+
+levelTitle.addEventListener('click', function () {
+    message("Selecione um nível:", 0, true);
+});
+
+ease.addEventListener('click', function (){
+    gameSettings = {
+        level: "ease",
+        range: {
+            min: 1,
+            max: 20,
+        },
+        numbersSorted: 1,
+        attempts: 7,
+    };
+    levelTitle.classList.add("selected");
+    notLevel.classList.remove("selected");
+});
+
+medium.addEventListener('click', function (){
+    gameSettings = {
+        level: "medium",
+        range: {
+            min: 1,
+            max: 30,
+        },
+        numbersSorted: 1,
+        attempts: 5,
+    };
+    levelTitle.classList.add("selected");
+    notLevel.classList.remove("selected");
+});
+
+hard.addEventListener('click', function (){
+    gameSettings = {
+        level: "hard",
+        range: {
+            min: 1,
+            max: 50,
+        },
+        numbersSorted: 1,
+        attempts: 3,
+    };
+    levelTitle.classList.add("selected");
+    notLevel.classList.remove("selected");
+});
+
+// Range
+rangeMin.addEventListener('input', function (e) {
+    if(!Number.isInteger(Number(e.target.value))){
+        message("Insira um número inteiro", 1, false);
+    }else{
+        if(!(Number(e.target.value) >= gameSettings.range.max)){
+            messages[1].innerHTML = "";
+            gameSettings.range.min = Number(e.target.value);
+        }else{
+            rangeMin.value = gameSettings.range.min;
+            message("Range Mínimo não pode ser maior que o Range Máximo", 1, false);
+        }
+    }   
+});
+
+rangeMax.addEventListener('input', function (e) {
+    if(!Number.isInteger(Number(e.target.value))){
+        message("Insira um número inteiro", 1, false);
+    }else{
+        if(!(Number(e.target.value) <= gameSettings.range.min)){
+            messages[1].innerHTML = "";
+            gameSettings.range.max = Number(e.target.value);
+        }else{
+            rangeMax.value = gameSettings.range.max;
+            message("Range Máximo não pode ser menor que o Range Mínimo", 1, false); 
+        }
+    }   
+});
+
+
+
+
+
 
 if(gameSettings.level === "ease") {
     
@@ -57,3 +157,13 @@ function bestTimeSave(){
 
 };
 // caso seja selecionado 
+
+function message(msg, number, temporary){
+    messages[number].innerHTML = msg;
+    messages[number].style.display = "block";
+    if(temporary){
+        setTimeout(function () {
+            messages[number].style.display = "none";
+        }, 4000);
+    }
+}
