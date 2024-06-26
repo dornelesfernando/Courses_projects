696323a1102c572ad94bff6b4217f9a37f021ae1
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-06-26 18:54:00 -0300

    Update main.js

--- Alura/Lógica de Programação/Medium/Jogo - Adivinhe o Número/js/main.js
+++ Alura/Lógica de Programação/Medium/Jogo - Adivinhe o Número/js/main.js
@@ -7,11 +7,3 @@ number.addEventListener('keyup', ({key}) => {
 play.addEventListener('click', function (e) {
     playGame();
 });
-
-rangeScale.addEventListener('mouseenter', (e) => {
-    rangeTitle.classList.add('Hover');
-});
-
-rangeScale.addEventListener('mouseover', (e) => {
-    rangeTitle.classList.remove('Hover');
-});
