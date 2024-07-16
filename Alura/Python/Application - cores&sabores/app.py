5a3d1b0a5aa3fb6b6b4f33c35858f8d1f7cc42b1
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-07-16 14:53:29 -0300

    Update app.py

--- Alura/Python/Application - cores&sabores/app.py
+++ Alura/Python/Application - cores&sabores/app.py
@@ -1,9 +1,39 @@
-print('Cores&Sabores')
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_nome_do_programa():
+    print("""
+        𝐂𝐨𝐫𝐞𝐬 & 𝐒𝐚𝐛𝐨𝐫𝐞𝐬
+        """)
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção', opcao_escolhida)
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    # os.system('clear') # no mac
+    print('Finalizando o app')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # opcao_escolhida = int(opcao_escolhida)
+
+    match opcao_escolhida:
+        case 1:
+            print('Cadastrar restaurante')
+        case 2:
+            print('Listar restaurante')
+        case 3:
+            print('Ativar restaurante')
+        case _:
+            finalizar_app()
+            
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
